61f05ac1650b6aa16b603c4d1d46f27397e4ee69
c21040245 <milliganEC@cardiff.ac.uk>
2022-11-19 15:51:35 +0000

    Added jpa support to customer entity

--- src/main/java/com/example/restaurantorderingsystem/domain/Customer.java
+++ src/main/java/com/example/restaurantorderingsystem/domain/Customer.java
@@ -1,12 +1,36 @@
 package com.example.restaurantorderingsystem.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Customer", uniqueConstraints = @UniqueConstraint(columnNames = "CustomerEmail"))
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "CustomerID")
     private Integer id;
+    @Column(name = "CustomerName")
     private String CustomerName;
+    @Column(name = "CustomerEmail")
     private String CustomerEmail;
+    @Column(name = "CustomerTelephoneNum")
     private String CustomerTelephoneNum;
+    @Column(name = "CustomerAddress")
     private String CustomerAddress;
+    @Column(name = "CustomerPassword")
     private String CustomerPassword;
-    
+    private Collection<UserRole> roles;
+
 }
